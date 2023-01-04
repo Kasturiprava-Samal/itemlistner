2e230cd82444d97c43d89274aa56d13572089e24
Kasturiprava-Samal <kasturipravasamal.official@gmail.com>
2023-01-04 18:17:11 +0530

    tsk7

---
+++ demo.js
@@ -0,0 +1,30 @@
+var itemList = document.querySelector('#items');
+//parent node
+console.log(itemList.parentNode);
+itemList.parentNode.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentNode.parentNode.parentNode);
+//parentElement
+console.log(itemList.parentElement);
+itemList.parentElement.style.backgroundColor = '#f4f4f4';
+console.log(itemList.parentElement.parentElement.parentElement);
+
+//child node
+console.log(itemList.childNodes);
+
+console.log(itemList.children);
+
+console.log(itemList.children[1]);
+itemList.children[1].style.backgroundColor ='yellow';
+
+console.log(itemList.firstChild);
+console.log(itemList.firstElementChild);
+itemList.firstElementChild.textContent = 'Hello 1';
+
+console.log(itemList.lastChild);
+console.log(itemList.lastElementChild);
+itemList.lastElementChild.textContent = 'Lat text one';
+// next sibling
+console.log(itemList.nextSibling);
+console.log(itemList.nextElementSibling);
+console.log(itemList.previousSibling);
+console.log(itemList.previousElementSibling);
